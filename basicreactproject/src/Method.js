9b99180490409e7abb079e3a59a45d4a951e478c
SansarTiwari <st9889477@gmail.com git config --global user.name SansarTiwari>
2023-12-19 15:08:58 +0530

    Third commit

--- basicreactproject/src/Method.js
+++ basicreactproject/src/Method.js
@@ -1,7 +1,11 @@
 function Method()
 {
     return (
-        <h2>First component of react</h2>
+        <>
+            <h2>First component of react</h2>
+            <h3>First function</h3>
+        </>
+      
     )
 }
 export default Method;
