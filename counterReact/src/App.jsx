fb41b42ff3677a92e9b1caca3620388d46abeb07
Sansar <you@example.com>
2024-01-14 12:00:28 +0530

    secondcommit

--- counterReact/src/App.jsx
+++ counterReact/src/App.jsx
@@ -1,23 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useState } from 'react';
 import './App.css'
 
 function App() {
 
-  let counter = 15;
+ let [counter,setcounter] =  useState(10)
+  //let counter = 15;
 
-  function addValue()
+ const addValue = () =>
   {
-    console.log("Value added",Math.random())
+    
+    //counter = counter + 1
+    if(counter<20)
+    {
+    setcounter(counter + 1);
+    document.getElementById("removecounter").innerHTML = "";
+    }
+    else
+    {
+      document.getElementById("addingcounter").innerHTML = "Maximum limit of counter: "+ counter;
+    }
   }
 
+  const removeValue = () =>{
+   // counter = counter - 1;
+   if(counter>0)
+    {
+    setcounter(counter - 1);
+    document.getElementById("addingcounter").innerHTML = " ";
+    }
+    else
+    {
+      document.getElementById("removecounter").innerHTML = "Minimum limit of counter: "+ counter;
+    }
+  }
   return (
     <>
       <h1>Counter React Project</h1>
       <h3>Counter Value: {counter}</h3>
       <button onClick={addValue}>Add value</button>
-      <button>remove value</button>
+      <h2 id="addingcounter"></h2>
+      <button onClick={removeValue}>remove value</button>
+      <h2 id="removecounter"></h2>
     </>
   )
 }
