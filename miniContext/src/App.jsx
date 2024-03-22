78240398baa39a0df386950aad0247415a5aa31b
Sansar <you@example.com>
2024-03-22 21:52:08 +0530

    miniContext api ,useContextHook

---
+++ miniContext/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import UserContext from './context/Usercontext'
+
+function App() {
+
+    return ( <
+        UserContext >
+        Hello, we are making Context Api <
+        /UserContext>
+    )
+}
+
+export default App
