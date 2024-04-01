f459d300027d67019efa8c25f12b8000716191b8
Sansar <you@example.com>
2024-04-01 21:50:26 +0530

    contextapi with local storage

--- context_apiLocalStorage/src/contexts/TodoContext.js
+++ context_apiLocalStorage/src/contexts/TodoContext.js
@@ -1,6 +1,16 @@
 import { createContext, useContext } from "react";
 
-export const TodoContext = createContext({})
+export const TodoContext = createContext({
+    todos: [{
+        id: 1,
+        todo: "Todos msg",
+        completed: false
+    }],
+    addTodo: (todo) => {},
+    updatedTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
 
 export const useTodo = () => {
     return useContext(TodoContext)
