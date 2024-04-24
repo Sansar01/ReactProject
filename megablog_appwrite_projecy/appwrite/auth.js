f6207ae3f5c02b5ab9afd05156b96e7eb4b37be1
SansarTiwari <st9889477@gmail.com git config --global user.name SansarTiwari>
2024-04-24 21:50:08 +0530

    auth service completed

--- megablog_appwrite_projecy/appwrite/auth.js
+++ megablog_appwrite_projecy/appwrite/auth.js
@@ -8,6 +8,61 @@ class AuthService{
     constructor()
     {
         this.client.setEndpoint(conf.appwriteUrl).setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email,password,name})
+    {
+        try {
+           const userAccount =   await this.account.create(Id.unique(),email,password,name);   
+             if(userAccount)
+             {
+                //call another method
+                return this.login({email,password});
+             }
+             else
+             {
+                return userAccount;
+             }
+        } 
+        catch (error) {
+            throw error;
+        }
+    }
+
+    async login(email, password)
+    {
+        try 
+        {
+           return await this.account.createEmailSession(email,password);    
+        } 
+        catch (error) {
+            
+        }
+    }
+
+    async getCurrentUser()
+    {
+        try {
+        return this.account.get();    
+        } 
+        catch (error) {
+            console.log("Appwrite service :: getCurrentUser:: error",error);
+            
+        }
+        return  null;
+    }
+
+    async logout()
+    {
+        try
+         {
+          return this.account.deleteSessions();  
+        } 
+        catch (error) {
+            console.log("Appwrite service :: logout:: error",error);
+            
+        }
     }
 }
 
